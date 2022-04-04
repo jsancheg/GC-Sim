40c9a9770c989de8a6f4214fbcb180b9d5b4b125
jsancheg <jalfre81@hotmail.com>
2022-04-04 16:58:19 +0100

    Update GC_Sim.R

--- GC_Sim.R
+++ GC_Sim.R
@@ -4,14 +4,6 @@ library(methods)
 # 
 rm(list = ls())
 
-movies <- setRefClass("movies", fields = list(name = "character",
-                                              leadActor = "character", rating = "numeric"))
-
-#now we can use the generator to create objects
-movieList <- movies(name = "Iron Man", 
-                    leadActor = "Robert downey Jr", rating = 7)
-movieList
-
 
 Peaks <- setRefClass("Peaks",fields = list(peak = "numeric",
                                    variance = "numeric",
@@ -19,13 +11,12 @@ Peaks <- setRefClass("Peaks",fields = list(peak = "numeric",
                                    lowerlimit = "numeric",
                                    upperlimit = "numeric") )
 
-peak1 <- Peaks(peak = 500,
-               variance = 2,
-               intensity = 20)
 
 peakset<-Peaks(peak =c(500,1100,1400,1750,1900,2300,2500),
                variance =c(2,2,1,1,1,1,1),
-               intensity = c(1,0.25,0.25,0.25,180,700,400))
+               intensity = c(1,0.25,0.25,0.25,180,700,400),
+               lowerlimit = 0,
+               upperlimit = 5000)
 
 
 
